d04762d4c4af189c93c2664420a150310a454e62
Albanir Neves <albanirneves@gmail.com>
2025-09-26 18:45:59 -0300

    update

--- supabase/functions/collage/index.ts
+++ supabase/functions/collage/index.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
 import { Image } from "https://deno.land/x/imagescript@1.3.0/mod.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.45.2";
-
+//update
 // ---------- ENV ----------
 const SUPABASE_URL = Deno.env.get("SUPABASE_URL")!;
 const SUPABASE_KEY =
